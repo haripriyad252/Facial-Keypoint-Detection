c30c145bbb8c68a1da2b554a28423f4528f36ff0
Haripriya Dharmala <30979528+haripriyad252@users.noreply.github.com>
2020-05-02 10:41:34 -0700

    Add files via upload
    Changed architecture and fixed size errors.

--- models.py
+++ models.py
@@ -14,22 +14,23 @@ class Net(nn.Module):
         
         ## TODO: Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
-        self.conv1 = nn.Conv2d(1,32,kernel_size=(4,4))
+        self.conv1 = nn.Conv2d(1,32,kernel_size=(4,4),padding=0)
         self.pool = nn.MaxPool2d(2,2,padding=0)
         self.dropout1 = nn.Dropout(0.1)
-        self.conv2 = nn.Conv2d(32,64,kernel_size=(3,3))
+        self.conv2 = nn.Conv2d(in_channels=32,out_channels=64,kernel_size=(3,3),padding=0)
         self.dropout2 = nn.Dropout(0.2)
-        self.conv3 = nn.Conv2d(64,128,kernel_size=(2,2))
+        self.conv3 = nn.Conv2d(in_channels=64,out_channels=128,kernel_size=(2,2),padding=0)
         self.dropout3 = nn.Dropout(0.3)
-        self.conv4 = nn.Conv2d(128,256,kernel_size=(1,1))
+        self.conv4 = nn.Conv2d(in_channels=128,out_channels=256,kernel_size=(1,1),padding=0)
         self.dropout4 = nn.Dropout(0.4)
         
         
         ## 2. It ends with a linear layer that represents the keypoints
 #         self.fc1 = nn.Linear(10,6400)
-        self.fc1 = nn.Linear(10,6400)
+        self.fc1 = nn.Linear(43264,1000) #works
         self.dropout5 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(6400,1000)
+#         self.fc2 = nn.Linear(6400,1000)
+        self.fc2 = nn.Linear(1000,1000)
         self.dropout6 = nn.Dropout(0.6)
         self.fc3 = nn.Linear(1000,136)
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
@@ -53,22 +54,30 @@ class Net(nn.Module):
         
         x = self.pool(F.relu(self.conv1(x)))
         x = self.dropout1(x)
+        print("the length after conv1",x.size())
         x = self.pool(F.relu(self.conv2(x)))
         x = self.dropout2(x)
+        print("length after conv2",x.size())
         x = self.pool(F.relu(self.conv3(x)))
         x = self.dropout3(x)
+        print("length after conv3",x.size())
         x = self.pool(F.relu(self.conv4(x)))
         x = self.dropout4(x)
+        print("length after conv4",x.size())
         
          #size of x is 256*13*13
-        x = x.view(-1, x.size(0)) #flattening the layer
-
+        print("length before flattening",x.size())
+#         x = x.view(-1, x.size(0)) #flattening the layer
+        x = x.view(x.size(0), -1)
+        print("THE SIZE OF X IS", x.size())
+#         x = x.view(-1, self.num_flat_features(x))
+        print("length after flattening",x.size())
         x = F.relu(self.fc1(x))
         x = self.dropout5(x)
         x = F.relu(self.fc2(x))
         x = self.dropout6(x)
         x = self.fc3(x)
         x = F.softmax(x, dim=1)
-        
+        print("length after softmax",x.size())
         # a modified x, having gone through all the layers of your model, should be returned
         return x
